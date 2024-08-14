7eea49d02221c75eb5d8f6a8e85d06a62fd76c6d
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-08-14 09:10:10 -0300

    estudo angular: property binding e attribute binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -9,11 +9,13 @@ import { Component } from '@angular/core';
 })
 export class TemplateBindingComponent {
   public name = "Dener Plz";
-  public age = 32;
+  public age = 37;
 
   public condition = this.age > 1 ? "Teste 1" : "Teste 2"
 
   public srcValue = "https://vidafullstack.com.br/wp-content/uploads/2022/04/Aprenda-o-que-e-Databinding-com-Angular-980x551.jpg"
 
   public isDisabled = true;
+
+  public isTextDecoration = this.age > 32 ? 'underline' : 'none';
 }
